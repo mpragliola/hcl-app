be3f67e8212b91d22df2b1a1f1a643d9d0d14855
Marco Pragliola <marcopragliola@gmail.com>
2017-10-28 12:55:38 +0200

    Implemented logger logic

--- src/app/ui/logger/logger.component.ts
+++ src/app/ui/logger/logger.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AttemptLoggerService } from '../../services/attempt-logger.service';
+
 @Component({
   selector: 'app-logger',
   templateUrl: './logger.component.html',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoggerComponent implements OnInit {
 
-  constructor() { }
+  guesses = [];
+  lastAttempt;
+  total = 0;
+  rightGuesses = 0;
+
+  constructor(
+      private persistence: AttemptLoggerService
+  ) { }
 
   ngOnInit() {
+      this.persistence
+        .updateStream$
+        .subscribe(attempt => {
+          this.guesses = this.persistence.attempts(10);
+          this.lastAttempt = attempt;
+          this.total = this.persistence.getTotal();
+          this.rightGuesses = this.persistence.getStats();
+        })
   }
 
 }
